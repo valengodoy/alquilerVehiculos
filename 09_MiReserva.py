5c258b163ae7800fd52391856fdf6bb265b68c57
julirojo <juliantabbita@gmail.com>
2025-05-24 13:34:16 -0300

    Funcionalidad cancelar reserva

--- 09_MiReserva.py
+++ 09_MiReserva.py
@@ -2,7 +2,7 @@ import streamlit as st
 from datetime import date
 import pandas as pd
 from functions.usuarios import obtener_usuario_actual, tiene_reserva
-from functions.reserva import *
+from functions.reserva import obtener_reserva, cancelar_reserva
 from functions.vehiculos import obtener_auto
 
 user = obtener_usuario_actual()
@@ -16,9 +16,29 @@ if user != None:
     if tiene_reserva(user.get("email")):
         reserva = obtener_reserva(user.get("email"))
         auto = obtener_auto(reserva.get("patente"))
-        st.image(f"imagenes/{auto.get('imagen')}", use_container_width=True)
+        
+        #Muestro info de la reserva
         st.error(f"**{auto.get('marca')} {auto.get('modelo')} {auto.get('año')} {auto.get('tipo')} 💲{auto.get('precio_dia')}**")
+        st.image(f"imagenes/{auto.get('imagen')}", use_container_width=True)
         st.info(f"Reserva desde el {reserva.get('fecha_inicio')} hasta el {reserva.get('fecha_fin')}")
+        
+        #Botones de gestion
+        cols = st.columns([2,1,2,1,2])
+        with cols[0]:
+            agregarConductor = st.button("Agregar conductor")
+        with cols[2]:
+            pagarReserva = st.button("Pagar reserva")
+        with cols[4]:
+            cancelar = st.button("Cancelar Reserva")
+            
+        #Boton cancelar
+        if cancelar:
+            inicio = pd.to_datetime(reserva["fecha_inicio"], format="%d/%m/%Y").date()
+            if inicio <= date.today():
+                st.error("La reserva no se puede cancelar por que el aquiler esta en progreso ❌")
+            else:
+                cancelar_reserva(reserva.get("id_reserva"))
+                st.success("Tu reserva ha sido cancelada")
     else:
         st.error('No tiene ninguna reserva ahora mismo ❌')
 else:
--- functions/reserva.py
+++ functions/reserva.py
@@ -15,9 +15,18 @@ def registrar_reserva(reserva):
     
 def guardar_reserva(df):
     df.to_csv(archivo_alquileres, index=False)
-    
+
 def obtener_reserva(email):
     df = cargar_reservas()
     reserva = df[df["usuario_id"] == email]
     return reserva.iloc[0].to_dict()
 
+def cancelar_reserva(id):
+    df = cargar_reservas()
+    idx = df[df["id_reserva"] == id].index
+    if len(idx) == 0:
+        return False  
+    df.at[idx[0], "estado"] = "cancelado"
+    df.to_csv("data/alquileres.csv", index=False)
+
+    return True
